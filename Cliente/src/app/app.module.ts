404336d24f6d06ca1f192e725044e37ec3f38872
Leonardo-Jaren <2022110517@udh.edu.pe>
2024-11-14 22:47:10 -0500

    prueba de crear evento

--- Cliente/src/app/app.module.ts
+++ Cliente/src/app/app.module.ts
@@ -20,7 +20,8 @@ import { HomeComponent } from './home/home.component';
 import { CalendarioComponent } from './calendario/calendario.component';
 import { SubscripcionesComponent } from './subscripciones/subscripciones.component';
 import { NavigationComponent } from './navigation/navigation.component'; // Módulo de diálogo de confirmación de PrimeNG
-import { FullCalendarModule } from '@fullcalendar/angular'; // Importa FullCalendar
+import { FullCalendarModule } from '@fullcalendar/angular'; // Importa FullCalendar --INSTALAR CALENDAR
+
 
 @NgModule({
   declarations: [
--- Cliente/src/app/eventos/eventos.component.ts
+++ Cliente/src/app/eventos/eventos.component.ts
@@ -12,10 +12,21 @@ export class EventosComponent {
   eventos: Evento[];
   constructor(private authService: AuthService, private cliente: ApiService) {}
 
+  isModalOpen: boolean = false;
+
   ngOnInit(): void {
     this.cliente.getEventos().subscribe(eventos => {
       this.eventos = eventos;
     });
+  }
+
+  openModal() {
+    this.isModalOpen = true;
+  }
+
+  // Método para cerrar el modal
+  closeModal() {
+    this.isModalOpen = false;
   }
 
   logout() {
